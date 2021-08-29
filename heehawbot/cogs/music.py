8e1f563db5b06f9a19ee234b7b842dbed1657339
Justin Schaare <jschaare16@gmail.com>
2021-08-29 02:31:39 -0400

    add other useful music commands

--- heehawbot/cogs/music.py
+++ heehawbot/cogs/music.py
@@ -16,13 +16,14 @@ FFMPEG_OPTS = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
 
 
 class YoutubeVideo:
-    def __init__(self, ctx) -> None:
-        video = self._extract(ctx)
+    def __init__(self, url, user) -> None:
+        video = self._extract(url)
         self.stream_url = video["formats"][0]["url"]
         self.video_url = video["webpage_url"]
         self.title = video["title"]
         self.uploader = video["uploader"] if "uploader" in video else ""
         self.thumbnail = video["thumbnail"] if "thumbnail" in video else None
+        self.user = user
 
     def _extract(self, url):
         with ytdl.YoutubeDL(YTDL_OPTS) as downloader:
@@ -36,6 +37,9 @@ class YoutubeVideo:
         emb = Embed(title=self.title, description=self.uploader, url=self.video_url)
         if self.thumbnail:
             emb.set_thumbnail(url=self.thumbnail)
+        emb.set_footer(
+            text=f"Queued by {self.user.name}", icon_url=self.user.avatar_url
+        )
         return emb
 
 
@@ -44,6 +48,14 @@ class GuildMusicManager:
         self.playlist = []
         self.playing = None
         self.volume = 1.0
+
+
+async def in_voice(ctx):
+    uv = ctx.author.voice
+    bv = ctx.guild.voice_client
+    if uv and bv and uv.channel and bv.channel and uv.channel == bv.channel:
+        return True
+    return False
 
 
 class Music(Cog):
@@ -74,7 +86,7 @@ class Music(Cog):
 
         vc.play(source, after=cleanup)
 
-    @commands.command()
+    @commands.command(aliases=["p"])
     @commands.guild_only()
     async def play(self, ctx, *, url):
         vc = ctx.guild.voice_client
@@ -82,9 +94,9 @@ class Music(Cog):
 
         if vc and vc.channel:
             try:
-                video = YoutubeVideo(url)
-            except:
-                print("error")
+                video = YoutubeVideo(url, ctx.author)
+            except ytdl.DownloadError as e:
+                self.bot.logger.error(f"`{type(e).__name__}: {e}`")
                 return
             manager.playlist.append(video)
             msg = await ctx.send(embed=video.embed())
@@ -92,23 +104,45 @@ class Music(Cog):
             if ctx.author.voice is not None and ctx.author.voice.channel is not None:
                 chan = ctx.author.voice.channel
                 try:
-                    video = YoutubeVideo(url)
-                except:
-                    print("error")
+                    video = YoutubeVideo(url, ctx.author)
+                except ytdl.DownloadError as e:
+                    self.bot.logger.error(f"`{type(e).__name__}: {e}`")
                     return
                 vc = await chan.connect()
                 self._play(vc, manager, video)
                 msg = await ctx.send(embed=video.embed())
             else:
-                print("error")
+                self.bot.logger.error("User must be in a voice channel")
                 return
+
+    @commands.command(aliases=["n"])
+    @commands.check(in_voice)
+    @commands.guild_only()
+    async def skip(self, ctx):
+        vc = ctx.guild.voice_client
+        vc.stop()
+
+    @commands.command()
+    @commands.check(in_voice)
+    @commands.guild_only()
+    async def clear(self, ctx):
+        manager = self._get_manager(ctx.guild)
+        manager.playlist = []
+
+        vc = ctx.guild.voice_client
+        vc.stop()
 
     @commands.command()
     @commands.guild_only()
-    async def skip(self, ctx):
+    async def queue(self, ctx):
         manager = self._get_manager(ctx.guild)
-        vc = ctx.guild.voice_client
-        vc.stop()
+        if len(manager.playlist) > 0:
+            msg = "Queue:\n"
+            for i, song in enumerate(manager.playlist):
+                msg += f"{i+1}: {song.title}\n"
+            await ctx.send(msg)
+        else:
+            await ctx.send("No songs in queue...")
 
 
 def setup(bot):
